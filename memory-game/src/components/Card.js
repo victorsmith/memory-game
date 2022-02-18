c9681918af959a0e28888b0567350fa0ac016b31
victorsmith <victor7smith@gmail.com>
2022-02-17 23:12:10 -0700

    basic functionality done

--- memory-game/src/components/Card.js
+++ memory-game/src/components/Card.js
@@ -3,14 +3,10 @@ import '../styles/Card.css'
 const Card = (props) => {
 
 	const cardData = props.cardData;
-	console.log(cardData);
-
 	const imgSrc = `images/${cardData.img}`
-	console.log(imgSrc);
 
     return (
 		<div className="card">
-			<h1>{cardData.id}</h1>
 			<h3>{cardData.name}</h3>
 			<img src={process.env.PUBLIC_URL + imgSrc} alt="pic" />
 		</div>
--- memory-game/src/components/Game.js
+++ memory-game/src/components/Game.js
@@ -1,31 +1,75 @@
 import '../App.css';
-import Header from './Header'
+import Header from './Header';
 import Gameboard from './Gameboard';
 import Footer from './Footer';
-import { v4 as uuid } from 'uuid'
+import Card from './Card';
 
+import { v4 as uuid } from 'uuid';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-
-function Game () {
-
-	const [score, SetScore] = useState(0);
-	const [highScore, SetHighScore] = useState(0);
-    const [gameCards, SetCards] = useState([
+function Game() {
+	const [score, setScore] = useState(0);
+	const [highScore, setHighScore] = useState(0);
+	const [gameCards, setCards] = useState([
 		{ name: 'Ramen', img: 'ramen.jpg', id: uuid() },
 		{ name: 'Sushi', img: 'sushi.jpg', id: uuid() },
 		{ name: 'Burger', img: 'burger.jpg', id: uuid() },
 		{ name: 'Steak', img: 'steak.jpg', id: uuid() },
 	]);
-	
+	const [cardsPlayed, setCard] = useState([]);
 
-    return (
-        <>
-            <Header score={score} highScore={highScore}/>
-            <Gameboard cards={gameCards}/>
-            {/* <Footer/> */}
-        </>
+	useEffect(() => {
+		console.log(cardsPlayed);
+
+		if (score > highScore) {
+			setHighScore(score);
+		}
+	}, [score, highScore]);
+
+	function pushCard(cardObject) {
+		const isDuplicate = checkDuplicate(cardObject);
+
+		if (isDuplicate) {
+			resetCardsPlayed();
+		} else {
+			setScore((prevScore) => prevScore + 1);
+			setCard(cardsPlayed.concat(cardObject));
+		}
+	}
+
+	function resetCardsPlayed() {
+		setCard([]);
+        setScore(0);
+	}
+
+	function checkDuplicate(obj) {
+		for (let i = 0; i < cardsPlayed.length; i++) {
+			if (cardsPlayed[i].id === obj.id) {
+				console.log('duplicate');
+				return true;
+			}
+			return false;
+		}
+
+		return false;
+	}
+
+	return (
+		<>
+			<Header score={score} highScore={highScore} />
+			{/* <Gameboard cards={gameCards}/> */}
+			<div className="gameboard">
+				{gameCards.map((card) => {
+					return (
+						<div onClick={() => pushCard(card)}>
+							<Card cardData={card} key={card.id} />;
+						</div>
+					);
+				})}
+			</div>
+			{/* <Footer/> */}
+		</>
 	);
 }
 
